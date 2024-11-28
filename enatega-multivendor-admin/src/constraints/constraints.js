28fbf26798c33d074ea4789e6bc019d03765e8ac
dilaannkilc <masterydev01@gmail.com>
2024-11-28 09:55:48 +0200

    added the condition check for the constraints as per review by coderabbit ai (Rider.jsx, constraints.js)

--- enatega-multivendor-admin/src/constraints/constraints.js
+++ enatega-multivendor-admin/src/constraints/constraints.js
@@ -74,6 +74,10 @@ const constraints = {
   username: {
     presence: {
       allowEmpty: false
+    },
+    format: {
+      pattern: /^[^\s]+$/,
+      message: '^Username cannot contain spaces'
     }
   },
   title: {
@@ -248,4 +252,17 @@ const constraints = {
 
 export const validateFunc = (value, constraint) => {
   return validate(value, { [constraint]: constraints[constraint] })
+}
+export const validateFuncForRider = (value, constraint) => {
+  const validationResult = validate(value, {
+    [constraint]: constraints[constraint]
+  })
+  if (validationResult !== undefined && validationResult !== null) {
+    if (!constraints[constraint]) {
+      return { isValid: false, errorMessage: 'Invalid constraint' }
+    }
+    return { isValid: false, errorMessage: validationResult[constraint][0] }
+  } else {
+    return { isValid: true, errorMessage: null }
+  }
 }
