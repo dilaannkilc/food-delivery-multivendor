ce7975cf3d979f4f1ee67888816b7b2a554d1844
dilaannkilc <masterydev01@gmail.com>
2024-12-05 09:00:33 +0200

    added default image and optional chaining

--- enatega-multivendor-app/src/screens/Menu/Menu.js
+++ enatega-multivendor-app/src/screens/Menu/Menu.js
@@ -134,7 +134,7 @@ function Menu({ route, props }) {
 
   useFocusEffect(() => {
     if (Platform.OS === 'android') {
-      StatusBar.setBackgroundColor(currentTheme.headerColor)
+      StatusBar.setBackgroundColor(currentTheme.newheaderColor)
     }
     StatusBar.setBarStyle(
       themeContext.ThemeValue === 'Dark' ? 'light-content' : 'dark-content'
@@ -317,6 +317,7 @@ function Menu({ route, props }) {
           search={''}
           setSearch={() => {}}
           newheaderColor={newheaderColor}
+          placeHolder={t('searchRestaurant')}
         />
         <Placeholder
           Animation={props => (
@@ -505,6 +506,7 @@ function Menu({ route, props }) {
                     setSearch={setSearch}
                     search={search}
                     newheaderColor={newheaderColor}
+                    placeHolder={t('searchRestaurant')}
                   />
                   <Filters
                     filters={filters}
