a4a058e8abcf83f3541aacd7ac0a2cb55c9eabc0
dilaannkilc <masterydev01@gmail.com>
2024-12-26 18:42:30 +0200

    edit food issue fixed

--- enatega-multivendor-web/src/screens/NewLogin/NewLogin.js
+++ enatega-multivendor-web/src/screens/NewLogin/NewLogin.js
@@ -114,7 +114,7 @@ function NewLogin() {
           error={Boolean(error)}
           helperText={error}
           variant="outlined"
-          defaultValue={"demo-customer@enatega.com"}
+          value={"demo-customer@enatega.com"}
           label="Email"
           type={"email"}
           fullWidth
