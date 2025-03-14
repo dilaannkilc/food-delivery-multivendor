f73f695aaf5207ed41577f1804534ba5f2ea9ff8
dilaannkilc <masterydev01@gmail.com>
2025-03-14 13:05:01 +0200

    removed unused code (constants.js, index.js etc)

--- enatega-multivendor-app/src/routes/index.js
+++ enatega-multivendor-app/src/routes/index.js
@@ -65,6 +65,10 @@ const Location = createStackNavigator()
 
 function Drawer() {
 
+  // useEffect(() => {
+  // throw new Error('My first Sentry error 3 - again');
+
+  // },[])
  
   return (
     <SideDrawer.Navigator drawerContent={(props) => <SideBar {...props} />}>
