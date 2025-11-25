fc90c35a6c3fd3391029bf71d5c09b53160b7e95
dilaannkilc <masterydev01@gmail.com>
2025-11-25 17:45:11 +0200

    feature: added clarity in store and rider app

--- enatega-multivendor-web/next.config.mjs
+++ enatega-multivendor-web/next.config.mjs
@@ -27,7 +27,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: 'upgrade-insecure-requests; media-src \'self\' https://*.amazonaws.com https://*.s3.amazonaws.com data: blob:'
+            value: 'upgrade-insecure-requests'
           },
           // Optional: Add other security headers
           {
