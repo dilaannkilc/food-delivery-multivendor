ecd6de8bc1a0a489df35097dfd9e5238f33077c3
dilaannkilc <masterydev01@gmail.com>
2025-11-30 11:06:39 +0200

    fixed: #1610

--- enatega-multivendor-app/src/components/Cart/styles.js
+++ enatega-multivendor-app/src/components/Cart/styles.js
@@ -1,7 +1,6 @@
 import { StyleSheet, Dimensions } from 'react-native'
 import { alignment } from '../../utils/alignment'
 import { scale } from '../../utils/scaling'
-import { textStyles } from '../../utils/textStyles'
 const { height } = Dimensions.get('window')
 
 const BACKDROP_HEIGHT = Math.floor(scale(height / 5))
@@ -53,15 +52,5 @@ export const useStyles = (theme) => StyleSheet.create({
         borderRadius: scale(5),
         flexDirection: theme?.isRTL ? 'row-reverse' : 'row',
         alignItems: 'center'
-    },
-    textInput: {
-        padding: scale(10),
-        ...textStyles.H4,
-        flex: 1,
-        color: theme.fontMainColor,
-        textAlign: theme?.isRTL ? 'right' : 'left'
-    },
-    clearButton: {
-        ...alignment.MRxSmall
     }
 })
